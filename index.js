6a89a6c02bc6b0728797bf4cb0cb01da79746e35
Alexa Bautista <alexa.bautista@genstudents.org>
2024-06-21 21:15:46 -0500

    File with logic completed.

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+const formMathFibo = document.getElementById("formMathFibo")
+const valueMathFibo = document.getElementById("valueMathFibo");
+const resultFibo = document.getElementById("resultFibo")
+const numMathFibo = document.getElementById("numMathFibo")
 
 
-// LOGICA
+//ESCUCHADOR
+formMathFibo.addEventListener("submit", (e)=>{
+    e.preventDefault();
 
-const numFibo = 10;
+    const numFibo = valueMathFibo.value
+   
+    // ES VALOR NUMERICO
+    if (isNaN(numFibo)) {
+        msgError.style.backgroundColor = "#ffd1d1";
+        return (msgError.textContent = "Debe Ingresar un número");
+      }
+    
+      //   EVALUAR SI EL VALOR ES UN NÚMERO POSITIVO
+      if (numFibo <= 0) {
+        msgError.style.backgroundColor = "#ffd1d1";
+        return (msgError.textContent =
+          "Debe ingresar un número positivo mayor a 0");
+      }
+    
+    //   //   MODIFICAR ESTILOS CUANDO NO SE PRESENTEN ERRORES
+      msgError.style.backgroundColor = "transparent";
+      msgError.textContent = "";
+    
+    // LOGICA
+
 let numA = 0;
 let numB = 1;
 let secuencia = 0;
@@ -18,6 +44,10 @@ for (let i = 0; i < numFibo; i++ ){
         numB = secuencia
     }
     result += secuencia + ", "
-        
 }
-console.log(result);
+
+
+
+resultFibo.textContent =result
+})
+
